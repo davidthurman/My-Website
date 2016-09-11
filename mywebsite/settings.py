5262f5be1fb4abdc9a0d1d7ebeb01cdb581bad5b
David Thurman <davidthurmanwork@gmail.com>
2016-09-11 01:07:47 -0400

    Set DEBUG to false

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -140,8 +140,9 @@ if os.getcwd() == '/app':
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    # Allow only Heroku to host the project.
+    ALLOWED_HOSTS = ['agile-anchorage-96310.herokuapp.com']
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
